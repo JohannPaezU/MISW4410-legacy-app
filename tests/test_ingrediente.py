2b86904fbaf421a811e38af5fe15c9089e495cd4
JohannPaezU <js.paezc1@uniandes.edu.co>
2024-02-22 21:21:24 -0500

    HU002 - Caso 2: Crear ingrediente con nombre de longitud invalida (Rojo)

--- tests/test_ingrediente.py
+++ tests/test_ingrediente.py
@@ -14,4 +14,8 @@ class IngredienteTestCase(unittest.TestCase):
         mensaje = self.logica.validar_crear_editar_ingrediente("", "gramos", "200", "Carulla")
         self.assertEqual(mensaje, "El nombre del ingrediente no puede ser vacío")
 
+    # Al crear un ingrediente con el campo "nombre" mayor a 50 caracteres, debe lanzar un mensaje de error.
+    def test_validar_crear_editar_ingrediente_nombre_con_longitud_invalida(self):
+        mensaje = self.logica.validar_crear_editar_ingrediente("Nombre invalido de ingrediente, cuenta con mas de 50 caracteres", "gramos", "200", "Carulla")
+        self.assertEqual(mensaje, "El nombre del ingrediente no puede tener más de 50 caracteres")
 
