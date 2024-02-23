2df8d2507f0bbdb707b09d6a43157f06ec2c0166
JohannPaezU <js.paezc1@uniandes.edu.co>
2024-02-22 21:24:53 -0500

    HU002 - Caso 3: Crear ingrediente con campo unidad vacio (Rojo)

--- tests/test_ingrediente.py
+++ tests/test_ingrediente.py
@@ -10,12 +10,17 @@ class IngredienteTestCase(unittest.TestCase):
         self.logica = None
     
     # Al crear un ingrediente con el campo "nombre" vacio, debe lanzar un mensaje de error.
-    def test_validar_crear_editar_ingrediente_nombre_vacio(self):
+    def test_validar_crear_editar_ingrediente_campo_nombre_vacio(self):
         mensaje = self.logica.validar_crear_editar_ingrediente("", "gramos", "200", "Carulla")
         self.assertEqual(mensaje, "El nombre del ingrediente no puede ser vacío")
 
     # Al crear un ingrediente con el campo "nombre" mayor a 50 caracteres, debe lanzar un mensaje de error.
-    def test_validar_crear_editar_ingrediente_nombre_con_longitud_invalida(self):
+    def test_validar_crear_editar_ingrediente_campo_nombre_con_longitud_invalida(self):
         mensaje = self.logica.validar_crear_editar_ingrediente("Nombre invalido de ingrediente, cuenta con mas de 50 caracteres", "gramos", "200", "Carulla")
         self.assertEqual(mensaje, "El nombre del ingrediente no puede tener más de 50 caracteres")
+
+    # Al crear un ingrediente con el campo "unidad" vacio, debe lanzar un mensaje de error.
+    def test_validar_crear_editar_ingrediente_campo_unidad_vacio(self):
+        mensaje = self.logica.validar_crear_editar_ingrediente("Papa", "", "200", "Carulla")
+        self.assertEqual(mensaje, "La unidad de medida del ingrediente no puede ser vacía")
 
