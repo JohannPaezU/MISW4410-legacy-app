fd0adc80183781ed23fc0a726faebc35783f99f6
JohannPaezU <js.paezc1@uniandes.edu.co>
2024-02-24 13:15:25 -0500

    HU009 - Caso 2: Listar recetas cuando si existe alguna en base de datos (Rojo)

--- tests/test_receta.py
+++ tests/test_receta.py
@@ -23,3 +23,13 @@ class RecetaTestCase(unittest.TestCase):
     def test_listar_recetas_sin_registros(self):
         recetas = self.logica.dar_recetas()
         self.assertEqual(len(recetas), 0)
+
+    # Validar que si hay recetas registradas en BD, se devuelva una lista con las recetas.
+    def test_listar_recetas_lista_llena(self):
+        consulta1 = self.logica.dar_recetas()
+        receta = Receta(nombre="Arroz con pollo", personas=4, calorias=500, preparacion="Hervir el arroz",
+                        tiempo="00:10:10")
+        session.add(receta)
+        session.commit()
+        consulta2 = self.logica.dar_recetas()
+        self.assertGreater(len(consulta2), len(consulta1))
