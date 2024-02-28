640c877586092c7631ca5a536b2fb943cdd6ab19
JohannPaezU <js.paezc1@uniandes.edu.co>
2024-02-28 13:18:20 -0500

    HU006 - Caso 2: Crear receta con tiempo de preparación vacio (Verde)

--- src/logica/Logica.py
+++ src/logica/Logica.py
@@ -19,6 +19,8 @@ class Logica(FachadaRecetario):
     def validar_crear_editar_receta(self, id_receta, receta, tiempo, personas, calorias, preparacion):
         if receta == "":
             return "El nombre de la receta no puede ser vacío"
+        if tiempo == "":
+            return "El tiempo de preparación no puede ser vacío"
         return None
 
     def crear_receta(self, receta, tiempo, personas, calorias, preparacion):
